55d4e36341a85513522473aa17c29ff061135946
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2024-10-04 19:46:26 +0500

    notebook added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='UzbekSpeechToText',
+    version='0.0.0',
+    author='Baxtiyor Mustafoyev',
+    author_email='baxtiyormustafoyev2006@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "UzbekSpeechToText"
+
+list_of_files = [
+    "data/.gitkeep",
+    f"notebooks/trials.ipynb",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/exception/__init__.py",
+    f"src/{project_name}/logger/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/main_utils.py",
+    "templates/index.html",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    if (not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+    else:
+        logging.info(f"{filename} is already created")
